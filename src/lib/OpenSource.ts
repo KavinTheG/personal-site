3ab16fc48bf06e3d38e467ae56bf60a7b7d65eb2
Kavin Gnanapandithan <kavin.balag@gmail.com>
2025-11-20 11:47:48 -0500

    Changed PR links to the commits

--- src/lib/OpenSource.ts
+++ src/lib/OpenSource.ts
@@ -17,7 +17,7 @@ const openSourceContributions: OpenSourceContribution[] = [
     prs: [
       {
         title: "[X86] Lower mathlib call ldexp into scalef when avx512 is enabled",
-        link: "https://github.com/llvm/llvm-project/pull/166839",
+        link: "https://github.com/llvm/llvm-project/commit/6c79cc7ff7901e9c8a3a4e924abd0e9dbfae039c",
         notes: [
           "Implemented custom lowering for ISD::FLDEXP to lower to VSCALEF/VSCALEFS instructions on AVX-512 targets",
           "Widened f16 vectors to f32 when FP16 is not available",  
@@ -26,7 +26,7 @@ const openSourceContributions: OpenSourceContribution[] = [
       },
       {
         title: "[DAG] Add ISD::VECTOR_COMPRESS handling in computeKnownBits/ComputeNumSignBits",
-        link: "https://github.com/llvm/llvm-project/pull/159692",
+        link: "https://github.com/llvm/llvm-project/commit/d46998b1dd1a2db10f4372a0ac8eced9c5b5a073",
         notes: [
           "Added case handling for ISD::VECTOR_COMPRESS in computeKnownBits/ComputeNumSignBits, improving KnownBits propagation across backends",
           "Added test cases for X86 AVX512, AArch64 SVE, and RISC-V RVV"
